05dbbf1c8e9706a33c179dad952fb146df01d968
omidarian <32985764+omidarian@users.noreply.github.com>
2020-10-30 13:31:48 -0700

    Prj2

--- welcome.php
+++ welcome.php
@@ -17,6 +17,10 @@
         <h2>Welcome: 
             <?php echo $_SESSION['username']?>
         </h2>
+        <!-- Form of Initialize Database-->
+        <form action="welcome.php" method="post">
+            <input type="submit" name="iniButton" id="iniDataButton" value="Initialize Database"/><br><br>
+        </form>
         <!-- Form of input data-->
         <form action="index.php" method="post">
             <input type="submit" id="logOutButton" value="Log Out"/>
@@ -24,6 +28,31 @@
         
     </div>
     <div id="resultDiv" align="center"></div>
+
+    <?php
+        if(isset($_POST['iniButton']))
+        {
+            $query = '';
+            $sqlScript = file('create.sql');
+            foreach ($sqlScript as $line)	{
+                
+                $startWith = substr(trim($line), 0 ,2);
+                $endWith = substr(trim($line), -1 ,1);
+                
+                if (empty($line) || $startWith == '--' || $startWith == '/*' || $startWith == '//') {
+                    continue;
+                }
+                    
+                $query = $query . $line;
+                if ($endWith == ';') {
+                    mysqli_query($con,$query) or die('<div class="error-response sql-import-response">Problem in executing the SQL query <b>' . $query. '</b></div>');
+                    $query= '';		
+                }
+            }
+            echo '<center>Initialize Database Successfull!</center>';
+        }
+
+    ?>
 
 </body>
 </html>
